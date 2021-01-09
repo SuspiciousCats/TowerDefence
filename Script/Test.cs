b6fe148e9980010c48617d8582507069d232b7c1
CreativeMetalCat <catguy228@gmail.com>
2021-01-09 20:03:27 +1000

    init

---
+++ Script/Test.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class Test : Node2D
+{
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Hello there");
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
